cbd5ddea9cc1426e396831eed71226a36f45a86e
Gourav <gourav@turka.tech>
2026-09-07 14:17:53 +0530

    undo last changes

--- src/bars/wrenches/bar-test-6-16mm.js
+++ src/bars/wrenches/bar-test-6-16mm.js
@@ -21,7 +21,6 @@ const slots = [
   { size: 16, thickness: 5.6 }, // 16-17 tool - PLACEHOLDER
 ];
 
-// Testing again the git backups
 const main = () => buildComb(slots);
 
 module.exports = { main };
